e6f7703defeef63dd9924089df4f13471c15a737
suren <siddu9626@gmail.com>
2025-09-16 16:10:56 +0530

    started with basic main method, datatype, identifier,literal

---
+++ src/Lab01/Aaramb.java
@@ -0,0 +1,17 @@
+package Lab01;
+
+public class Aaramb {
+    public static void main(String[] args)
+    {
+        int a=25;
+//        DIL
+//        D= datatype
+//        I= identifier
+//        L=literal
+        /*identifiers can be variable/method/classes/interface*/
+//        here a acts as identifier
+//        25 acts as a literal
+//        variable is a container and it can vary at any point in the program
+        System.out.println(a);
+    }
+}

c2249970a57efdd4ac620f596080119fc1d0661c
suren <siddu9626@gmail.com>
2025-09-17 20:49:08 +0530

    Types of print statements

---
+++ src/Lab01/Formatting.java
@@ -0,0 +1,21 @@
+package Lab01;
+
+public class Formatting {
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.printf("the value of given 9*1= %d%n", n);
+        System.out.printf("the value of given 9*2= %d%n", n*2);
+        System.out.printf("the value of given 9*3= %d%n", n *3);
+        System.out.printf("the value of given 9*4=%d%n", n * 4);
+        System.out.printf("the value of given 9*5=%d%n", n * 5);
+        System.out.printf("the value of given 9*6=%d%n", n * 6);
+        System.out.printf("the value of given 9*7=%d%n", n * 7);
+        System.out.printf("the value of given 9*8=%d%n", n * 8);
+        System.out.printf("the value of given 9*9=%d%n", n * 9);
+        System.out.printf("the value of given 9*10=%d%n", n * 10);
+    }
+}
+
+
+
+
